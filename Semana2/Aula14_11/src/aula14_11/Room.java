5bc708c0acd5e3e42d69664e3a6ed083b846150d
David Vinícius Pereira Lima <dvplima.cic@uesc.br>
2023-11-14 18:09:31 -0300

    atualização classe room

--- Semana2/Aula14_11/src/aula14_11/Room.java
+++ Semana2/Aula14_11/src/aula14_11/Room.java
@@ -1,5 +1,46 @@
 package aula14_11;
 
 public class Room {
+	private double length;
+	private double breadth;
+	private double height;
 
+	public Room(double length, double breadth, double height) {
+		this.length = length;
+		this.breadth = breadth;
+		this.height = height;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getBreadth() {
+		return breadth;
+	}
+
+	public void setBreadth(double breadth) {
+		this.breadth = breadth;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double calculateArea() {
+		return length * breadth;
+	}
+
+	public double calculateVolume() {
+		return length * breadth * height;
+	}
+	
 }
