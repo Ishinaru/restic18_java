4853c41cdd726d7a62d608d871d8ea9b60b40a0c
Ishinaru <dvplima.cic@uesc.br>
2023-12-19 11:38:28 -0300

    update po-04

--- Semana6/aula-15.12.23/src/op_04/src/po_04/calculadora/Calculadora.java
+++ Semana6/aula-15.12.23/src/op_04/src/po_04/calculadora/Calculadora.java
@@ -1,23 +1,48 @@
-package op_04;
+package po_04.calculadora;
 
-import op_03.DivisaoPorZeroException;
+import java.util.ArrayList;
 
-public class Calculadora {
+public class Calculadora{
     public int soma(int x, int y){return x+y;}
     public double soma(double x, double y){return x+y;}
+
+    public int soma(ArrayList<Integer>num){
+        int soma = 0;
+        for(int i: num){
+            soma+=i;
+        }
+        return soma;
+    }
     public int subtracao(int x, int y){return x-y;}
     public double subtracao(double x, double y){return x-y;}
+    public int subtracao(ArrayList<Integer>num){
+        int sub = 0;
+        for(int i:num){
+            sub-=i;
+        }
+        return sub;
+    }
     public int multiplicacao(int x, int y){return x*y;}
     public double multiplicacao(double x, double y){return x*y;}
-    public int divisao(int x, int y)throws DivisaoPorZeroException {
-        if(y==0)
-            throw new DivisaoPorZeroException("Divisão por zero, erro!");
+
+    public int multiplicacao(ArrayList<Integer>num){
+        int mul = 1;
+        for (int i:num){
+            mul*=i;
+        }
+        return mul;
+    }
+    public int divisao(int x, int y)throws DivisaoPorZeroException{
+            if(y==0)
+                throw new DivisaoPorZeroException("Divisão por zero, erro!");
+            else
         return x/y;
     }
     public double divisao(double x, double y)throws DivisaoPorZeroException{
         if(y==0)
             throw new DivisaoPorZeroException("Divisão por zero, erro!");
-        return x/y;
+        else
+            return x/y;
     }
 
     public int divisao(String x, String y) throws DivisaoPorZeroException {
@@ -37,4 +62,15 @@ public class Calculadora {
 
         return div;
     }
+
+    public int divisao(ArrayList<Integer>num){
+        int div = 1;
+        for (int i: num){
+            div/=i;
+        }
+        return div;
+    }
+
 }
+
+
