a7a6241a2f6c991800771b31131a47a3b38502d6
Ishinaru <dvplima.cic@uesc.br>
2024-01-13 13:28:37 -0300

    add classe jornada na po-06

---
+++ Semana8/aula_12.1.24/src/po_06/exercicio4/Jornada.java
@@ -0,0 +1,4 @@
+package po_06.exercicio4;
+
+public class Jornada {
+}
--- Semana8/aula_12.1.24/src/po_06/exercicio4/Trajeto.java
+++ Semana8/aula_12.1.24/src/po_06/exercicio4/Trajeto.java
@@ -2,7 +2,48 @@ package po_06.exercicio4;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Trajeto {
-    List<Trecho>trajetos = new ArrayList<>();
+    List<Trecho>trechosPercorridos = new ArrayList<>();
+
+    public void addTrechos(Trecho trecho){
+        trechosPercorridos.add(trecho);
+    }
+    private List<Trecho> listarTrechos(){
+        return trechosPercorridos;
+    }
+    public String toString(){
+        String str = "";
+        for (Trecho t : trechosPercorridos) {
+            str += t.toString() + "\n\t\t";
+        }
+        return "Trajeto:\n\t\t" + str;
+    }
+
+    public void printTrajeto(){
+        System.out.println(this.toString());
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List <Trecho> trechos = new ArrayList<>();
+        Trajeto trajeto = new Trajeto();
+
+        for(int i = 0 ; i < 3; i++){
+            System.out.print("Ponto Origem: ");
+            String nome = sc.nextLine();
+            PontoParada pOrigem = new PontoParada(nome);
+            System.out.print("Ponto Destino: ");
+            nome = sc.nextLine();
+            PontoParada pDestino = new PontoParada(nome);
+            System.out.print("Tempo de viagem (em minutos): ");
+            int tempo = sc.nextInt();
+            sc.nextLine();
+            Trecho trecho = new Trecho(pOrigem, pDestino, tempo);
+            trajeto.addTrechos(trecho);
+        }
+        trajeto.printTrajeto();
+
+    }
 }
