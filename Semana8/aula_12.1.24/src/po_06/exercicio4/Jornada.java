704b244de9a070ca0a3e3083dd940b2a46c5de39
Ishinaru <dvplima.cic@uesc.br>
2024-01-14 12:59:21 -0300

    Incrementando classe Jornada da po-06

--- Semana8/aula_12.1.24/src/po_06/exercicio4/Jornada.java
+++ Semana8/aula_12.1.24/src/po_06/exercicio4/Jornada.java
@@ -1,5 +1,13 @@
 package po_06.exercicio4;
 
+import java.time.LocalDateTime;
+
 public class Jornada {
+    Motorista motoristaDaJornada;
+    Cobrador cobradorDaJornada;
+    Veiculo veiculoDaJornada;
+    LocalDateTime horaInicioJornada;
+    LocalDateTime horaFinalJornada;
+
 
 }
