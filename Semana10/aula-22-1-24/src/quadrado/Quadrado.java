5fb5b8de2e515112485280d46bd90d8745f76ded
David Vinícius Pereira Lima <dvplima.cic@uesc.br>
2024-01-22 12:10:25 -0300

    atualização classe Quadrado

--- Semana10/aula-22-1-24/src/quadrado/Quadrado.java
+++ Semana10/aula-22-1-24/src/quadrado/Quadrado.java
@@ -12,8 +12,66 @@ public class Quadrado {
         this.estado = estado;
     }
 
+    public void inverte(int k){
+        if(estado.charAt(k) == 'O')
+          estado.charAt(k). = 'X';
+        else
+            estado.
+    }
     public void clique(int k){
-
+        switch (k){
+            case 1:
+                inverte(0);
+                inverte(1);
+                inverte(3);
+                break;
+            case 2:
+                inverte(0);
+                inverte(1);
+                inverte(2);
+                inverte(4);
+                break;
+            case 3:
+                inverte(1);
+                inverte(2);
+                inverte(5);
+                break;
+            case 4:
+                inverte(0);
+                inverte(3);
+                inverte(4);
+                inverte(6);
+                break;
+            case 5:
+                inverte(1);
+                inverte(3);
+                inverte(4);
+                inverte(5);
+                inverte(7);
+                break;
+            case 6:
+                inverte(2);
+                inverte(4);
+                inverte(5);
+                inverte(8);
+                break;
+            case 7:
+                inverte(3);
+                inverte(6);
+                inverte(7);
+                break;
+            case 8:
+                inverte(4);
+                inverte(6);
+                inverte(7);
+                inverte(8);
+                break;
+            case 9:
+                inverte(5);
+                inverte(7);
+                inverte(8);
+                break;
+        }
     }
 
 
