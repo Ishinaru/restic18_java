726c02bda31bad6ff9cd1bf7496381e6ec91e6e7
David Vinícius Pereira Lima <dvplima.cic@uesc.br>
2024-01-22 11:49:28 -0300

    implementando classe Quadrado

--- Semana10/aula-22-1-24/src/quadrado/Quadrado.java
+++ Semana10/aula-22-1-24/src/quadrado/Quadrado.java
@@ -1,4 +1,20 @@
 package quadrado;
 
 public class Quadrado {
+    private String estado;
+    public Quadrado(String estado){
+        this.estado = estado;
+    }
+    public String getEstado(){
+        return estado;
+    }
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+
+    public void clique(int k){
+
+    }
+
+
 }
