3eeb3134e86ee46ef7e3cc4c34d57e4a1000a19a
Ishinaru <dvplima.cic@uesc.br>
2024-01-26 17:16:53 -0300

    atualizações da aula de hoje - sql

---
+++ Semana10/aula-26.1.24/src/testeBD/dao/DAO.java
@@ -0,0 +1,27 @@
+package testeBD.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAO {
+    private static final String URL = "jdbc:mysql://uu3vun18liwwkjwh:zvCEtHpzXKerU2yI0HAU@bhhp0ojrrzhhxw6t4gya-mysql.services.clever-cloud.com:3306/bhhp0ojrrzhhxw6t4gya";
+    private static final String USER = "uu3vun18liwwkjwh";
+    private static final String PASSWD ="zvCEtHpzXKerU2yI0HAU";
+    public static Connection conectar(){
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection(URL, USER, PASSWD);
+            System.out.println("Conexão feita com sucesso!!!!");
+        }   catch (SQLException e){
+            System.out.println("Erro de conexão");
+        }
+        return con;
+    }
+
+    public static void main(String[] args) {
+        DAO.conectar();
+
+    }
+}
+
