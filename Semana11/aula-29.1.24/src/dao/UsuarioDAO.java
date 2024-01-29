0beab09b72b5d4c19b0f014ef970eb5069084045
Ishinaru <dvplima.cic@uesc.br>
2024-01-29 14:31:50 -0300

    select usuarios

--- Semana11/aula-29.1.24/src/dao/UsuarioDAO.java
+++ Semana11/aula-29.1.24/src/dao/UsuarioDAO.java
@@ -2,8 +2,11 @@ package dao;
 
 import usuario.Usuario;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class UsuarioDAO {
 
@@ -24,4 +27,25 @@ public class UsuarioDAO {
             return false;
         }
     }
+
+    public static ArrayList<Usuario> readAll(){
+        Connection conn = DAO.conectar();
+        String query = "SELECT Login, Senha, Email FROM Usuario";
+        ArrayList<Usuario> listaUsuarios = new ArrayList<>();
+        try{
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs =  ps.executeQuery();
+            while (rs.next()){
+                Usuario usuario = new Usuario();
+                usuario.setLogin(rs.getString("Login"));
+                usuario.setSenha(rs.getString("Senha"));
+                usuario.setEmail(rs.getString("Email"));
+                listaUsuarios.add(usuario);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return listaUsuarios;
+    }
+
 }
--- Semana11/aula-29.1.24/src/program/TestaUsuario.java
+++ Semana11/aula-29.1.24/src/program/TestaUsuario.java
@@ -3,12 +3,25 @@ package program;
 import dao.UsuarioDAO;
 import usuario.Usuario;
 
+import java.util.ArrayList;
+
 public class TestaUsuario {
     public static void main(String[] args) {
+        /* Insert
         Usuario usuario = new Usuario();
         usuario.setLogin("Ishinaru");
         usuario.setSenha("senhateste");
         usuario.setEmail("ishinaru");
         UsuarioDAO.create(usuario);
+        */
+
+        ArrayList<Usuario> usuarios = UsuarioDAO.readAll();
+        for (Usuario usuario : usuarios){
+            System.out.println(usuario.getLogin());
+            System.out.println(usuario.getSenha());
+            System.out.println(usuario.getEmail());
+            System.out.println("============================");
+        }
+
     }
 }
