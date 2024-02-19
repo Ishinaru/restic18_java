2ec13de7385daef3a82c1ac96285b304ec4f379c
Ishinaru <dvplima.cic@uesc.br>
2024-02-19 11:15:58 -0300

    implementação das classes do pacote modelo: Aeroporto, Piloto, ModeloAeronave

--- Semana13/Aeroporto/src/main/java/com/controleaeroporto/aeroporto/modelo/Piloto.java
+++ Semana13/Aeroporto/src/main/java/com/controleaeroporto/aeroporto/modelo/Piloto.java
@@ -1,4 +1,48 @@
 package com.controleaeroporto.aeroporto.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Piloto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String Nome;
+    private String NumBreve;
+
+    public Piloto() {
+    }
+
+    public Piloto(Long id, String nome, String numBreve) {
+        Id = id;
+        Nome = nome;
+        NumBreve = numBreve;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getNumBreve() {
+        return NumBreve;
+    }
+
+    public void setNumBreve(String numBreve) {
+        NumBreve = numBreve;
+    }
 }
