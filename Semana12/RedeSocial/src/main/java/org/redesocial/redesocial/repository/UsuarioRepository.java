bf8ec12aa706c513834a6cce202c6aebe964408c
Ishinaru <dvplima.cic@uesc.br>
2024-02-19 10:23:53 -0300

    adicionando package repository e interface UsuarioRepository

---
+++ Semana12/RedeSocial/src/main/java/org/redesocial/redesocial/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package org.redesocial.redesocial.repository;
+
+import org.redesocial.redesocial.modelo.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
