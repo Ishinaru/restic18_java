d99d8226c52d942bab092dd9874eb3288cd4f267
Ishinaru <dvplima.cic@uesc.br>
2024-02-26 11:54:02 -0300

    adicionando aeroporto

--- Semana13/Aeroporto/src/main/java/com/controleaeroporto/aeroporto/controller/ListaAeroportosController.java
+++ Semana13/Aeroporto/src/main/java/com/controleaeroporto/aeroporto/controller/ListaAeroportosController.java
@@ -1,10 +1,15 @@
 package com.controleaeroporto.aeroporto.controller;
 
+import com.controleaeroporto.aeroporto.controller.forms.AeroportosForm;
 import com.controleaeroporto.aeroporto.modelo.Aeroporto;
 import com.controleaeroporto.aeroporto.repository.AeroportoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -14,10 +19,16 @@ public class ListaAeroportosController {
     private AeroportoRepository aeroportoRepository;
     @RequestMapping("/listaaeroporto")
     public List<Aeroporto>listaAeroportos(){
-<<<<<<< HEAD
-        return (<Aeroporto>) aeroportoRepository.findAll();
-=======
         return aeroportoRepository.findAll();
->>>>>>> ad985cd0e139b8d4212254fa669fb49a684b3632
     }
+    @PostMapping("/{aeroportos}")
+    public RequestEntity<?>inserirAeroporto(@RequestBody UriComponentsBuilder uriBuilder, AeroportosForm af){
+        Aeroporto aeroporto = af.criarAeroporto();
+        aeroportoRepository.save(aeroporto);
+        AeroportoDTO aeroportoDTO = AeroportoDTO(aeroporto);
+        uriBuilder.path("/aeroporto/{id}");
+        URI uri = uriBuilder.buildAndExpand(aeroporto.getId()).toUri();
+        return ResponseEntity.created(uri).body(aeroportoDTO);
+    }
+
 }
