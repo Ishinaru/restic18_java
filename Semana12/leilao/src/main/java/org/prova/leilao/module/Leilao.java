07c6421140152de5970fbeb965fda9964e1982f5
Ishinaru <dvplima.cic@uesc.br>
2024-02-29 15:08:58 -0300

    implementando classe Leilao

--- Semana12/leilao/src/main/java/org/prova/leilao/module/Leilao.java
+++ Semana12/leilao/src/main/java/org/prova/leilao/module/Leilao.java
@@ -6,4 +6,62 @@ public class Leilao {
     private Double valorMinimo;
     private boolean aberto;
 
+    public Leilao() {
+    }
+
+    public Leilao(Long id, String descricao, Double valorMinimo) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valorMinimo = valorMinimo;
+        this.aberto = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValorMinimo() {
+        return valorMinimo;
+    }
+
+    public void setValorMinimo(Double valorMinimo) {
+        this.valorMinimo = valorMinimo;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public String toString(){
+        if (this.aberto)
+            return "Leilão nº "+id+"\n" +
+                    "Descrição: "+descricao+"\n" +
+                    "Valor Mínimo: "+valorMinimo+"\n"+
+                    "Status: Aberto";
+        else
+            return "Leilão nº "+id+"\n" +
+                    "Descrição: "+descricao+"\n" +
+                    "Valor Mínimo: "+valorMinimo+"\n"+
+                    "Status: Fechado";
+    }
+
+    public void mostrarLeilao(){
+        System.out.println(toString());
+    }
 }
