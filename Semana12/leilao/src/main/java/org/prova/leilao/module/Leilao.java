0705577447f5166f10c07a5fcebe00f415a9f811
Ishinaru <dvplima.cic@uesc.br>
2024-02-29 15:12:42 -0300

    adicionando flags na classe Leilão

--- Semana12/leilao/src/main/java/org/prova/leilao/module/Leilao.java
+++ Semana12/leilao/src/main/java/org/prova/leilao/module/Leilao.java
@@ -1,6 +1,13 @@
 package org.prova.leilao.module;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Leilao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String descricao;
     private Double valorMinimo;
