0cff448687b653561c6f93e363f6afea9ae3068d
Ishinaru <dvplima.cic@uesc.br>
2024-02-29 16:41:28 -0300

    implementando classe ConcorrentRepository e ConcorrenteController

---
+++ Semana13/leilao/src/main/java/org/prova/leilao/controller/ConcorrenteController.java
@@ -0,0 +1,7 @@
+package org.prova.leilao.controller;
+
+import org.prova.leilao.repository.ConcorrenteRepository;
+
+public class ConcorrenteController {
+    private ConcorrenteRepository concorrenteRepository;
+}
---
+++ Semana13/leilao/src/main/java/org/prova/leilao/repository/ConcorrenteRepository.java
@@ -0,0 +1,7 @@
+package org.prova.leilao.repository;
+
+import org.prova.leilao.module.Concorrente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConcorrenteRepository extends JpaRepository<Concorrente, Long> {
+}
