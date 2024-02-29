b5afc43c7c7e13977e9bfd5d9dbdaa005b1b6307
Ishinaru <dvplima.cic@uesc.br>
2024-02-29 16:36:04 -0300

    implementando classe LanceRepository e LanceControllr

---
+++ Semana13/leilao/src/main/java/org/prova/leilao/controller/LanceController.java
@@ -0,0 +1,7 @@
+package org.prova.leilao.controller;
+
+import org.prova.leilao.repository.LanceRepository;
+
+public class LanceController {
+    private LanceRepository lanceRepository;
+}
---
+++ Semana13/leilao/src/main/java/org/prova/leilao/repository/LanceRepository.java
@@ -0,0 +1,7 @@
+package org.prova.leilao.repository;
+
+import org.prova.leilao.module.Lance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanceRepository extends JpaRepository<Lance, Long> {
+}
